dfbbb32a9ac76a3c1c64cad7d87975cb7a029cf7
Juan Antonio <juanjamfw@gmail.com>
2018-04-03 12:03:38 +0200

    Configuración de la conexión a la base de datos

---
+++ dbconfig.php
@@ -0,0 +1,14 @@
+<?php
+//DB detalles
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'tienda';
+
+//Conexión a la base de datos
+$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+if ($db->connect_error) {
+    die("Unable to connect database: " . $db->connect_error);
+}
+?>
