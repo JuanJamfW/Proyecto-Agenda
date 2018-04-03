227a84fa20dfbfc96406164679cdc57d8a55d20c
Juan Antonio <juanjamfw@gmail.com>
2018-04-03 12:31:32 +0200

    Añadir y crear los estilos de la página

--- index.php
+++ index.php
@@ -13,27 +13,37 @@ include 'dbConfig.php';
 </head>
 
 <body>
-<h1>Lista de tareas a realizar</h1>
-<h2>Añadir nueva tarea</h2>
-    <form action="AddTask.php" method="post">
-        Descripcion:
-        <br>
-        <input type="text" name="description" >
-        <br>
-        Fecha limite(dd/mm/yyyy):
-        <br>
-        <input type="date" name="date" >
-        <br>
-        <label for="pr">Prioridad</label> <br/>
-        <select id="pr" name="pr">
-            <option value="" selected="selected">- selecciona -</option>
-            <option value="Alta">Alta</option>
-            <option value="Media">Media</option>
-            <option value="Baja">Baja</option>
-        </select>
-        <br>
-        <input type="submit" value="Guardar">
-    </form>
+<div class="titulo"><h1>Lista de tareas a realizar</h1></div>
+<div class="subtitulo"><h2>Añadir nueva tarea</h2></div>
+    <div class="tarea">
+        <form action="" method="post">
+
+            <div class="descripcion">Descripcion:
+                <br>
+                <input type="text" name="description" >
+            </div>
+            <br>
+            <div class="fecha">
+                Fecha limite(dia/mes/año):
+                <br>
+                <input type="date" name="date" >
+            </div>
+            <br>
+            <div class="prioridad">
+                <label for="pr">Prioridad</label> <br/>
+                <select id="pr" name="pr">
+                    <option value="" selected="selected">- selecciona -</option>
+                    <option value="Alta">Alta</option>
+                    <option value="Media">Media</option>
+                    <option value="Baja">Baja</option>
+                </select>
+            </div>
+            <br>
+                <div class="boton">
+                <input type="submit" value="Guardar">
+            </div>
+        </form>
+    </div>
 <br>
 </body>
 </html>
